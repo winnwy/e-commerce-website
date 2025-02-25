d9816e7fa53b6f5f4fae9e51be170887ad2f743d
Winn Z <winnwyz@gmail.com>
2025-02-25 14:01:16 +1100

    Add root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import '@/assets/styles/globals.css'
+import "@/assets/styles/globals.css";
 
-const inter = Inter({subsets:['latin']});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Prostore",
@@ -16,11 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
