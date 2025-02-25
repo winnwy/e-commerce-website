e89fb078bd99dd37e85d1a67679ef36f163296ed
Winn Z <winnwyz@gmail.com>
2025-02-25 14:40:23 +1100

    Add header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,14 @@
+import Header from "@/components/shared/header";
+
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-        <div className="flex h-screen flex-col">
-            <main className="flex-1 wrapper">
-                {children}
-            </main>
-        </div>
-    );
-  }
-  
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main className="flex-1 wrapper">{children}</main>
+    </div>
+  );
+}
