1752a782ab95a7320479f10ad15893753f3cd57e
winnie <winnwyz@gmail.com>
2025-03-01 19:54:01 +1100

    Use pascal naming for components

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ModeToggle from "./mode-toggle";
+import ModeToggle from "./ModeToggle";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { ShoppingCart, UserIcon } from "lucide-react";
