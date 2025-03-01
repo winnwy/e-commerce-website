9371460d36d46f9b08461c3da06da5c5efca5919
winnie <winnwyz@gmail.com>
2025-03-01 20:45:41 +1100

    Add product cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,16 @@
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/ProductList";
+
 const Homepage = () => {
-  return (<>Prostore</>  );
-}
- 
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
+};
+
 export default Homepage;
