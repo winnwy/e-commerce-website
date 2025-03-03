e2aab2674b4567fb0ef48569bdf084b2a471b53c
winnie <winnwyz@gmail.com>
2025-03-03 13:54:42 +1100

    Add product details page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -13,3 +13,10 @@ export async function getLatestProducts() {
 
   return convertToPlainObject(data);
 }
+
+// Get single product by its slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: {slug: slug},
+  })
+}
