f48d9c24e2c7010e173eca7c21c1c3d6ef7618cd
winnie <winnwyz@gmail.com>
2025-03-03 12:49:59 +1100

    Add zod validation and type inference

--- components/shared/product/ProductCard.tsx
+++ components/shared/product/ProductCard.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 import Image from "next/image";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import ProductPrice from "./ProductPrice";
+import { Product } from "@/types";
 
-function ProductCard({ product }: { product: any }) {
+function ProductCard({ product }: { product: Product }) {
   return (
     <Card className="w-full max-w-sm">
       <CardHeader className="p-0 items-center">
--- components/shared/product/ProductList.tsx
+++ components/shared/product/ProductList.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ProductCard from "./ProductCard";
+import { Product } from "@/types";
 
 function ProductList({
   data,
   title,
   limit,
 }: {
-  data: any;
+  data: Product[];
   title?: string;
   limit?: number;
 }) {
@@ -16,7 +17,7 @@ function ProductList({
       <h2 className="h2-bold mb-4">{title}</h2>
       {limitedData.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {limitedData.map((product: any) => (
+          {limitedData.map((product: Product) => (
             <ProductCard key={product.slug} product={product} />
           ))}
         </div>
